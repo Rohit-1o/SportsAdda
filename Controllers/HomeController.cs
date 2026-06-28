4e643269fc0d666576283f41d2dd40ca8531bd87
Rohit Goud <rohitgoud254@gmail.com>
2026-06-28 15:08:16 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsAdda.Models;
+using System.Diagnostics;
+
+namespace SportsAdda.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
